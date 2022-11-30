c544bbdcf98364c1024e2bed3676e564d924ebb0
hellkirl <kirillprivalov04@icloud.com>
2022-11-30 15:50:15 +0300

    homework01

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -69,35 +69,21 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
         "z",
     ]
 
-    digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
     ciphertext = ""
-    cipher_list = []
-    cipher = []
     for i in plaintext:
         if i in uppercase:
             index = uppercase.index(i)
             encryption = (index + shift) % 26
-            cipher_list.append(encryption)
             newletter = uppercase[encryption]
-            cipher.append(newletter)
+            ciphertext += newletter
         if i in lowercase:
             index = lowercase.index(i)
             encryption = (index + shift) % 26
-            cipher_list.append(encryption)
             newletter = lowercase[encryption]
-            cipher.append(newletter)
-        if i in digits:
-            index = digits.index(i)
-            encryption = index
-            cipher_list.append(encryption)
-            newletter = digits[encryption]
-            cipher.append(newletter)
-        if i not in uppercase and i not in lowercase and i not in digits:
-            cipher.append(i)
+            ciphertext += newletter
+        if i not in uppercase and i not in lowercase:
+            ciphertext += i
 
-    for i in cipher:
-        ciphertext += i
     return ciphertext
 
 
@@ -114,7 +100,6 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
     uppercase = [
         "A",
         "B",
@@ -172,33 +157,20 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
         "y",
         "z",
     ]
+    plaintext = ""
 
-    digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
-    decipher_list = []
-    decipher = []
     for i in ciphertext:
         if i in uppercase:
             index = uppercase.index(i)
             decryption = (index - shift) % 26
-            decipher_list.append(decryption)
             newletter = uppercase[decryption]
-            decipher.append(newletter)
+            plaintext += newletter
         if i in lowercase:
             index = lowercase.index(i)
             decryption = (index - shift) % 26
-            decipher_list.append(decryption)
             newletter = lowercase[decryption]
-            decipher.append(newletter)
-        if i in digits:
-            index = digits.index(i)
-            decryption = index
-            decipher_list.append(decryption)
-            newletter = digits[decryption]
-            decipher.append(newletter)
-        if i not in uppercase and i not in lowercase and i not in digits:
-            decipher.append(i)
+            plaintext += newletter
+        if i not in uppercase and i not in lowercase:
+            plaintext += i
 
-    for i in decipher:
-        plaintext += i
     return plaintext
