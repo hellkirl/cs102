56846336901d03a195c9dc90dbb97cfd90d2490e
hellkirl <kirillprivalov04@icloud.com>
2022-12-14 18:12:15 +0300

    homework02

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -38,7 +38,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    result = [values[i : i + n] for i in range(0, len(values), n)]
+    result = [values[i: i + n] for i in range(0, len(values), n)]
     return result
 
 
@@ -78,7 +78,7 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
     corner = (pos[0] // 3 * 3, pos[1] // 3 * 3)
-    list_of_lists = [grid[i + corner[0]][corner[1] : corner[1] + 3] for i in range(0, 3)]
+    list_of_lists = [grid[i + corner[0]][corner[1]: corner[1] + 3] for i in range(0, 3)]
     return [item for sublist in list_of_lists for item in sublist]
 
 
@@ -120,7 +120,7 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
         3. Для каждого возможного значения:
             3.1. Поместить это значение на эту позицию
             3.2. Продолжить решать оставшуюся часть пазла
-            
+
     >>> grid = read_sudoku('puzzle1.txt')
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
