0a03e74d85ac461a8c35158b0a82f76c3cb098b3
hellkirl <kirillprivalov04@icloud.com>
2023-03-10 17:03:18 +0300

    hw4

---
+++ homework04/life_gui.py
@@ -0,0 +1,36 @@
+import pygame
+from life import GameOfLife
+from pygame.locals import *
+from ui import UI
+
+
+class GUI(UI):
+    def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
+        super().__init__(life)
+
+    def draw_lines(self) -> None:
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                             (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                             (0, y), (self.width, y))
+
+    def draw_grid(self) -> None:
+        # Copy from previous assignment
+        pass
+
+    def run(self) -> None:
+        pygame.init()
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color('white'))
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+            self.draw_lines()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
