6c1b190c47ef6f5d54edc3898701ae64b38fec4e
Kirill Privalov <110428918+hellkirl@users.noreply.github.com>
2023-07-01 14:04:29 +0300

    Delete homework09 directory

--- homework09/vkapi/config.py
+++
@@ -1,7 +0,0 @@
-# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
-
-VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
-    "access_token": "vk1.a.MlL-6r6ESiZICrc74uruvoIdew8tZDp4ew3kCUyqtXSYfNI9a84YGVkpDlq8OHfTnDI5GK-h5ptL28a8KgvFpIJTL7Th0GQ48-Np0OU2JiYkcHPg1OhFJbQaRj4pOj2TEV6VlK8wTVJX5X62lsAXWJhfvEuvnCQVDAzT3zflEOzN8VA2XHDXx5sqfAgoKLse",
-    "version": "5.126",
-}
